1a327c4354f3a5e73e2305cd5cce3a32fd23d964
caroAM22 <caalvarezmu@unal.edu.co>
2024-02-26 20:56:02 -0500

    Update lab_02

--- preguntas.py
+++ preguntas.py
@@ -181,12 +181,10 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    tbl0_sorted = (
-        tbl0.groupby("_c1")["_c2"]
-        .apply(lambda x: ":".join(sorted(x.astype(str))))
-        .reset_index(name="_c2")
+    tbl0_pivot = tbl0.pivot_table(
+        index="_c1", values="_c2", aggfunc=lambda x: ":".join(sorted(x.astype(str)))
     )
-    return tbl0_sorted
+    return tbl0_pivot
 
 
 def pregunta_11():
